259687c920d4de0ae09a713744cedfda613411ee
Jonny Lin <jonnylin@ubermedia.com>
2022-02-02 12:59:05 -0800

    Updated logging

--- index.js
+++ index.js
@@ -7,18 +7,13 @@ const { exec } = require('child_process');
 fastify.register(require('fastify-cors'), {});
 fastify.register(require('fastify-raw-body'));
 
-fastify.get('/simple-cd', async (request, reply) => {
-  return reply.status(403).send();
-});
-
 fastify.post('/simple-cd', async (request, reply) => {
+  console.log(JSON.stringify(request, null, 2));
   if (!('X-Hub-Signature-256' in request.headers)) return reply.status(403).send();
   const signature = Buffer.from(request.headers['X-Hub-Signature-256'] || '', 'utf-8');
-  
+
   const hmac = crypto.createHmac('sha256', process.env.SECRET);
   const digest = Buffer.from(`sha256=${hmac.update(request.rawBody).digest('hex')}`, 'utf-8');
-
-  console.log(JSON.stringify(request, null, 2));
 
   console.log(signature);
   console.log(digest);
