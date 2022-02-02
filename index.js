a20b68ad32bfd43b234ff132760ca578c126e088
Jonny Lin <jonnylin@ubermedia.com>
2022-02-02 12:12:40 -0800

    First test

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fastify = require('fastify')({ logger: true });
 require('dotenv').config();
 const crypto = require('crypto');
+const config = require('config.json');
+const { exec } = require('child_process');
 
 fastify.register(require('fastify-cors'), {});
 fastify.register(require('fastify-raw-body'));
@@ -17,6 +19,8 @@ fastify.post('/github', async (request, reply) => {
   if (signature.length !== digest.length || !crypto.timingSafeEqual(digest, signature)) return reply.status(403).send();
 
   console.log(JSON.stringify(request.body, null, 2));
+  exec(`echo "This works"`);
+
   return reply.status(200).send();
 });
 
